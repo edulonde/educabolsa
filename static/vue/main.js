4e4bfc0ca23c37d1312cc8a4d5f8b0ba2890cc8b
Edu Londe <72423464+edulonde@users.noreply.github.com>
2024-03-25 20:47:17 -0300

    add conceitos intermediarios

--- static/vue/main.js
+++ static/vue/main.js
@@ -9,9 +9,14 @@ const app = Vue.createApp({
             showConceitosBasicos_03: false,
             showConceitosBasicos_04: false,
             showConceitosBasicos_05: false,
-
-
-
+            showConceitosInter_01: false,
+            showConceitosInter_02: false,
+            showConceitosInter_03: false,
+            showConceitosInter_04: false,
+            showConceitosAvanc_01: false,
+            showConceitosAvanc_02: false,
+            showConceitosAvanc_03: false,
+            showConceitosAvanc_04: false,
 
         }
     },
